1d51a89cafa0fab07bf044aee5ae733e8eb2e463
Jay Patrick Cano <0x3EF8@gmail.com>
2022-04-07 20:16:31 +0800

    Add files via upload

---
+++ jobs.php
@@ -0,0 +1,222 @@
+<optgroup label="Administration, business and management">
+<option value='Administrative assistant'>Administrative assistant</option>
+<option value='Business analyst'>Business analyst</option>
+<option value='Business continuity specialist'>Business continuity specialist</option>
+<option value='Business development manager'>Business development manager</option>
+<option value='Civil service executive officer'>Civil service executive officer</option>
+<option value='Company secretary'>Company secretary</option>
+<option value='Compliance officer'>Compliance officer</option>
+<option value='Legal secretary'>Legal secretary</option>
+<option value='Local government officer'>Local government officer</option>
+</optgroup>
+
+<optgroup label="Management consultant">
+<option value='Medical secretary'>Medical secretary</option>
+</optgroup>
+
+<optgroup label="Computing and ICT">
+<option value='App developer'>App developer</option>
+<option value='AR/VR programmer'>AR/VR programmer</option>
+<option value='Ethical hacker'>Ethical hacker</option>
+<option value='Forensic computer analyst'>Forensic computer analyst</option>
+<option value='Game designer'>Game designer</option>
+<option value='Games developer'>Games developer</option>
+<option value='Games tester'>Games tester</option>
+<option value='Helpdesk professional'>Helpdesk professional</option>
+<option value='Infrastructure engineer'>Infrastructure engineer</option>
+<option value='IT project analyst'>IT project analyst</option>
+<option value='IT support technician'>IT support technician</option>
+<option value='IT trainer'>IT trainer</option>
+<option value='Machine learning engineer'>Machine learning engineer</option>
+<option value='Network manager'>Network manager</option>
+<option value='Office equipment service technician'>Office equipment service technician</option>
+<option value='Robotics engineer'>Robotics engineer</option>
+<option value='Software developer'>Software developer</option>
+<option value='Software engineer'>Software engineer</option>
+<option value='Solution architect'>Solution architect</option>
+<option value='Systems analyst'>Systems analyst</option>
+<option value='Virtual reality designer'>Virtual reality designer</option>
+<option value='Web developer'>Web developer</option>
+<option value='Web editor'>Web editor</option>
+</optgroup>
+
+<optgroup label="Construction and building">
+<option value='Architect'>Architect</option>
+<option value='Architectural technician'>Architectural technician</option>
+<option value='Architectural technologist'>Architectural technologist</option>
+<option value='Bricklayer'>Bricklayer</option>
+<option value='Builders merchant'>Builders merchant</option>
+<option value='Civil engineer'>Civil engineer</option>
+<option value='Civil engineering technician'>Civil engineering technician</option>
+<option value='Clerk of works'>Clerk of works</option>
+<option value='Commercial energy assessor'>Commercial energy assessor</option>
+<option value='Construction manager'>Construction manager</option>
+<option value='Construction operative'>Construction operative</option>
+<option value='Construction plant mechanic'>Construction plant mechanic</option>
+<option value='Construction plant operator'>Construction plant operator</option>
+</optgroup>
+
+
+<optgroup label="Design, arts and crafts">
+<option value='Animator'>Animator</option>
+<option value='Art gallery curator'>Art gallery curator</option>
+</optgroup>
+
+<optgroup label="Dressmaker">
+<option value='Exhibition designer'>Exhibition designer</option>
+<option value='Fashion designer'>Fashion designer</option>
+<option value='Fine artist'>Fine artist</option>
+</optgroup>
+
+<optgroup label="Education and training">
+<option value='Careers adviser'>Careers adviser</option>
+<option value='Classroom assistant'>Classroom assistant</option>
+<option value='Community education coordinator'>Community education coordinator</option>
+<option value='Early years teacher'>Early years teacher</option>
+<option value='EFL teacher'>EFL teacher</option>
+<option value='Further education lecturer'>Further education lecturer</option>
+<option value='Higher education lecturer'>Higher education lecturer</option>
+<option value='Learning support assistant'>Learning support assistant</option>
+<option value='Learning technologist'>Learning technologist</option>
+</optgroup>
+
+<optgroup label="Financial services">
+<option value='Accountant- Management'>Accountant- Management</option>
+<option value='Accounting technician'>Accounting technician</option>
+<option value='Accounts assistant'>Accounts assistant</option>
+<option value='Actuary'>Actuary</option>
+<option value='Bank manager'>Bank manager</option>
+<option value='Banking customer service adviser'>Banking customer service adviser</option>
+<option value='Bookkeeper'>Bookkeeper</option>
+<option value='Credit manager'>Credit manager</option>
+<option value='Economist'>Economist</option>
+<option value='Finance analyst'>Finance analyst</option>
+<option value='Financial adviser'>Financial adviser</option>
+<option value='Insurance account manager'>Insurance account manager</option>
+<option value='Insurance broker'>Insurance broker</option>
+</optgroup>
+
+<optgroup label="Healthcare">
+<option value='Health visitor'>Health visitor</option>
+<option value='Healthcare assistant'>Healthcare assistant</option>
+<option value='Hospital doctor'>Hospital doctor</option>
+<option value='Hospital porter'>Hospital porter</option>
+<option value='Medical physicist'>Medical physicist</option>
+<option value='Midwife'>Midwife</option>
+<option value='Music therapist'>Music therapist</option>
+<option value='Nurse'>Nurse</option>
+<option value='Optometrist'>Optometrist</option>
+<option value='Orthoptist'>Orthoptist</option>
+<option value='Pathologist'>Pathologist</option>
+<option value='Pharmacist'>Pharmacist</option>
+<option value='Pharmacy technician'>Pharmacy technician</option>
+</optgroup>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<option value='Region I - Ilocos Region'>Region I - Ilocos Region</option>
+<option value='Region II - Cagayan Valley'>Region II - Cagayan Valley</option>
+<option value='Region III - Central Luzon'>Region III - Central Luzon</option>
+<option value='Region IV-A - CALABARZON'>Region IV-A - CALABARZON</option>
+<option value='Region IV-B - MIMAROPA Region'>Region IV-B - MIMAROPA Region</option>
+<option value='Region V - Bicol Region'>Region V - Bicol Region</option>
+<option value='Region VI - Western Visayas'>Region VI - Western Visayas</option>
+<option value='Region VII - Central Visayas'>Region VII - Central Visayas</option>
+<option value='Region VIII - Eastern Visayas'>Region VIII - Eastern Visayas</option>
+<option value='Region IX - Zamboanga Peninsula'>Region IX - Zamboanga Peninsula</option>
+<option value='Region X - Northern Mindanao'>Region X - Northern Mindanao</option>
+<option value='Region XI - Davao Region'>Region XI - Davao Region</option>
+<option value='Region XII - SOCCSKSARGEN'>Region XII - SOCCSKSARGEN</option>
+<option value='Region XIII - Caraga'>Region XIII - Caraga</option>
+<option value='NCR - National Capital Region'>NCR - National Capital Region</option>
+<option value='CAR - Cordillera Administrative Region'>CAR - Cordillera Administrative Region</option>
+<option value='BARMM - Bangsamoro Autonomous Region in Muslim Mindanao'>BARMM - Bangsamoro Autonomous Region in Muslim Mindanao</option>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
